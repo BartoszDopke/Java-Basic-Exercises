d7bc9ebaacea40e878a26570c7e17d0ea29364cd
BartoszDopke <33840765+BartoszDopke@users.noreply.github.com>
2019-12-19 15:18:47 +0100

    SpeedConverter

---
+++ SpeedConverter.java
@@ -0,0 +1,19 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometeresPerHour) {
+        if(kilometeresPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometeresPerHour/1.609);
+    }
+
+    public static void printConversion(double kilometeresPerHour) {
+        if(kilometeresPerHour < 0) {
+            System.out.println("Invalid Value");;
+        } else {
+            long milesPerHour = toMilesPerHour(kilometeresPerHour);
+            System.out.println(kilometeresPerHour + " km/h = " + milesPerHour + " mil/h");
+        }
+    }
+}
+
