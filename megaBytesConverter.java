664c00fdf4c4d00786c1d6aef6659129cc128a5d
BartoszDopke <33840765+BartoszDopke@users.noreply.github.com>
2019-12-19 15:17:55 +0100

    MegaBytesConverter

---
+++ megaBytesConverter.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printMegaBytesAndKiloBytes(5000);
+    }
+
+    public static void printMegaBytesAndKiloBytes (int kiloBytes)
+    {
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        }
+        else{
+            int megaBytes = kiloBytes/1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + kiloBytes % 1024 + " KB");
+        }
+    }
+}
